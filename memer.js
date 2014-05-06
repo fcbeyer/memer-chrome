d7f96618cecb6e03f445b18549833edeec88fda7
Rick Beyer <rick.beyer@gmail.com>
2014-05-06 14:35:09 -0400

    updated code to fix some bugs

--- memer.js
+++ memer.js
@@ -31,58 +31,63 @@ function handleText(textNode)
 {
 	//var v = textNode.nodeValue;
 	var v = textNode.parentElement.innerHTML;
-	switch(v.match(/\b\w+\.jpg\b/g).toString()) {
-		case "notsureifserious.jpg":
-			var imgURL = chrome.extension.getURL("images/notsureifserious.png");
-			v = v.replace(/\bnotsureifserious.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "citationneeded.jpg":
-			var imgURL = chrome.extension.getURL("images/citation_needed.png");
-			v = v.replace(/\bcitationneeded.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "quizzicaldog.jpg":
-			var imgURL = chrome.extension.getURL("images/quizzicaldog.png");
-			v = v.replace(/\qquizzicaldog.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "whynotboth.jpg":
-			var imgURL = chrome.extension.getURL("images/whynotboth.png");
-			v = v.replace(/\bwhynotboth.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "youdontsay.jpg":
-			var imgURL = chrome.extension.getURL("images/youdontsay.png");
-			v = v.replace(/\byoudontsay.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "thisiswhywecanthavenicethings.jpg":
-			var imgURL = chrome.extension.getURL("images/thisiswhywecanthavenicethings.png");
-			v = v.replace(/\bthisiswhywecanthavenicethings.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "wtfisthisshit.jpg":
-			var imgURL = chrome.extension.getURL("images/wtfisthisshit.png");
-			v = v.replace(/\bwtfisthisshit.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "wat.jpg":
-			var imgURL = chrome.extension.getURL("images/wat.png");
-			v = v.replace(/\bwat.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "fistofangrygod.jpg":
-			var imgURL = chrome.extension.getURL("images/fistofangrygod.png");
-			v = v.replace(/\bfistofangrygod.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		case "icame.jpg":
-			var imgURL = chrome.extension.getURL("images/icame.png");
-			v = v.replace(/\bicame.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-			textNode.parentElement.innerHTML = v;
-			break;
-		default:
-			break;
+	var matches = v.match(/\b\w+\.jpg\b/g);
+	if (matches) {
+		for (var c = 0; c < matches.length; c++) {
+			switch(matches[c]) {
+				case "notsureifserious.jpg":
+					var imgURL = chrome.extension.getURL("images/notsureifserious.png");
+					v = v.replace(/\bnotsureifserious.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "citationneeded.jpg":
+					var imgURL = chrome.extension.getURL("images/citation_needed.png");
+					v = v.replace(/\bcitationneeded.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "quizzicaldog.jpg":
+					var imgURL = chrome.extension.getURL("images/quizzicaldog.png");
+					v = v.replace(/\qquizzicaldog.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "whynotboth.jpg":
+					var imgURL = chrome.extension.getURL("images/whynotboth.png");
+					v = v.replace(/\bwhynotboth.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "youdontsay.jpg":
+					var imgURL = chrome.extension.getURL("images/youdontsay.png");
+					v = v.replace(/\byoudontsay.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "thisiswhywecanthavenicethings.jpg":
+					var imgURL = chrome.extension.getURL("images/thisiswhywecanthavenicethings.png");
+					v = v.replace(/\bthisiswhywecanthavenicethings.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "wtfisthisshit.jpg":
+					var imgURL = chrome.extension.getURL("images/wtfisthisshit.png");
+					v = v.replace(/\bwtfisthisshit.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "wat.jpg":
+					var imgURL = chrome.extension.getURL("images/wat.png");
+					v = v.replace(/\bwat.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "fistofangrygod.jpg":
+					var imgURL = chrome.extension.getURL("images/fistofangrygod.png");
+					v = v.replace(/\bfistofangrygod.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				case "icame.jpg":
+					var imgURL = chrome.extension.getURL("images/icame.png");
+					v = v.replace(/\bicame.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
+					textNode.parentElement.innerHTML = v;
+					break;
+				default:
+					break;
+			}
+		}
 	}
 }
