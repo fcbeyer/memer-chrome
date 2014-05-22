4b0715b93a08872077093544ba17d7b6d824f9d5
Rick Beyer <rick.beyer@gmail.com>
2014-05-22 11:43:39 -0400

    added a new jquery plugin to fix my dom corruption issues. completely rewrote my extension so its awesome now! renamed the citationneeded image

--- memer.js
+++ memer.js
@@ -1,88 +1,14 @@
-walk(document.body);
+var memeList = buildMemeList();
+var memeRegExp = new RegExp("\b" + memeList + "\b",'g');
+$("body *").replaceText(memeRegExp, getImgUrl);
 
-function walk(node)
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-
-	var child, next;
-
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+function buildMemeList() {
+	var supportedMemes = ["notsureifserious.jpg","citationneeded.jpg","quizzicaldog.jpg","whynotboth.jpg","youdontsay.jpg","stoppedreadingthere.jpg",
+							"thisiswhywecanthavenicethings.jpg","wtfisthisshit.jpg","wat.jpg","fistofangrygod.jpg","icame.jpg"];
+	return supportedMemes.map(function(meme) {return "(" + meme + ")";}).join("|");
 }
 
-function handleText(textNode)
-{
-	var v = textNode.parentElement.innerHTML;
-	var matches = v.match(/\b\w+\.jpg\b/g);
-	var imgURL;
-	if (matches) {
-		for (var c = 0; c < matches.length; c++) {
-			switch(matches[c]) {
-				case "notsureifserious.jpg":
-					imgURL = chrome.extension.getURL("images/notsureifserious.png");
-					v = v.replace(/\bnotsureifserious.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "citationneeded.jpg":
-					imgURL = chrome.extension.getURL("images/citation_needed.png");
-					v = v.replace(/\bcitationneeded.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "quizzicaldog.jpg":
-					imgURL = chrome.extension.getURL("images/quizzicaldog.png");
-					v = v.replace(/\qquizzicaldog.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "whynotboth.jpg":
-					imgURL = chrome.extension.getURL("images/whynotboth.png");
-					v = v.replace(/\bwhynotboth.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "youdontsay.jpg":
-					imgURL = chrome.extension.getURL("images/youdontsay.png");
-					v = v.replace(/\byoudontsay.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "thisiswhywecanthavenicethings.jpg":
-					imgURL = chrome.extension.getURL("images/thisiswhywecanthavenicethings.png");
-					v = v.replace(/\bthisiswhywecanthavenicethings.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "wtfisthisshit.jpg":
-					imgURL = chrome.extension.getURL("images/wtfisthisshit.png");
-					v = v.replace(/\bwtfisthisshit.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "wat.jpg":
-					imgURL = chrome.extension.getURL("images/wat.png");
-					v = v.replace(/\bwat.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "fistofangrygod.jpg":
-					imgURL = chrome.extension.getURL("images/fistofangrygod.png");
-					v = v.replace(/\bfistofangrygod.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "icame.jpg":
-					imgURL = chrome.extension.getURL("images/icame.png");
-					v = v.replace(/\bicame.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				case "stoppedreadingthere.jpg":
-					imgURL = chrome.extension.getURL("images/stoppedreadingthere.png");
-					v = v.replace(/\bstoppedreadingthere.jpg\b/g, '<img src=' + imgURL + ' class="image memer-image">');
-					break;
-				default:
-					break;
-			}
-		}//end of for loop
-		textNode.parentElement.innerHTML = v;
-	}
+function getImgUrl(memeMatch){
+	memeMatch = memeMatch.replace(".jpg",".png");
+	return '<img src=' + chrome.extension.getURL("images/"+memeMatch)  + ' class="image memer-image">';
 }
